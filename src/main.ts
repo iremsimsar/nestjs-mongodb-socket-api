7d8d5e02c5eb3241b1b5c47758cf557e51d6876f
iremsimsar <irem.simsar@tsoft.com.tr>
2022-05-10 23:28:37 +0300

    project initial

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core'
+
+export class AppModule {}
+async function main() {
+  const app = await NestFactory.create(AppModule);
+  const port: number = parseInt(process.env.PORT);
+  await app.listen(port);
+}
+main();
